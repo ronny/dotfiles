722d1fd66abab56cd9e070026758319fd168a905
Ronny Haryanto <ronny@haryan.to>
2024-10-06 18:39:32 +1100

    fish changes

--- home/private_dot_config/private_fish/config.fish
+++ home/private_dot_config/private_fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    #starship init fish | source
+  # Commands to run in interactive sessions can go here
+
+  set -U fish_greeting ""
 end
 
 #####################################################################
