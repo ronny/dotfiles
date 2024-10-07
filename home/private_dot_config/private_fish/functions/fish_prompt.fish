5c60fd1395c784a3712f55fd3ee416623ce4aeb3
Ronny Haryanto <ronny@haryan.to>
2024-10-07 11:36:21 +1100

    shorter prompt

--- home/private_dot_config/private_fish/functions/fish_prompt.fish
+++ home/private_dot_config/private_fish/functions/fish_prompt.fish
@@ -13,7 +13,7 @@ function fish_prompt
 
   # Color the prompt differently when we're root
   set -l color_cwd $fish_color_cwd
-  set -l suffix '>'
+  set -l suffix '»'
   if functions -q fish_is_root_user; and fish_is_root_user
       if set -q fish_color_cwd_root
           set color_cwd $fish_color_cwd_root
@@ -33,6 +33,7 @@ function fish_prompt
   set -l statusb_color (set_color $bold_flag $fish_color_status)
   set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
-  echo -n -s (prompt_login)' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
+  #echo -n -s (prompt_login)' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
+  echo -n -s (set_color $color_cwd) (prompt_pwd) $normal ' ' (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
 
 end
